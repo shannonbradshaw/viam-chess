02566e66d4157dd1db50da1892e8dd16af8d8455
Eliot Horowitz <eliothorowitz@gmail.com>
2026-01-15 15:56:51 -0500

    don't enable tracking if not configured

--- common.go
+++ common.go
@@ -3,6 +3,7 @@ package viamchess
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 
@@ -13,12 +14,16 @@ import (
 var family = resource.ModelNamespace("erh").WithFamily("viam-chess")
 
 func enableTracing() {
-	exporter, err := otlptracegrpc.New(context.Background())
-	if err != nil {
-		fmt.Printf("can't enable tracing: %v", err)
+	if os.Getenv("OTEL_SERVICE_NAME") == "" {
+		fmt.Println("no OTEL_SERVICE_NAME, not enabling tracing")
 	} else {
-		err := trace.SetProvider(context.Background())
-		fmt.Printf("error setting new trace provider: %v", err)
-		trace.AddExporters(exporter)
+		exporter, err := otlptracegrpc.New(context.Background())
+		if err != nil {
+			fmt.Printf("can't enable tracing: %v", err)
+		} else {
+			err := trace.SetProvider(context.Background())
+			fmt.Printf("error setting new trace provider: %v", err)
+			trace.AddExporters(exporter)
+		}
 	}
 }
